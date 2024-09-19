ad7e3dae66d49501105e19a2fb668a493077f6be
yukuri611 <yukiiw611@gmail.com>
2024-09-19 13:44:46 +0900

    単項プラスとマイナスに対応

--- ykr_cc.c
+++ ykr_cc.c
@@ -96,7 +96,9 @@ struct Node {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
+
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
     Node *node = calloc(1, sizeof(Node));
@@ -127,16 +129,26 @@ Node *expr() {
 }
 
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
     for (;;) {
         if (consume('*')) {
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         } else if (consume('/')) {
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         } else {
             return node;
         }
     }
+}
+
+Node *unary() {
+    if (consume('+')) {
+        return primary();
+    }
+    if (consume('-')) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
 }
 
 Node *primary() {
